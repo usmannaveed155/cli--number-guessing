8847741fbf0519bf140709cdd52fcb4228255643
usman <usmannaveed155@gmail.com>
2024-04-18 23:23:57 +0500

    added a console

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ import inquirer from "inquirer"
 
  const randomNumber = Math.floor(Math.random() * 6 + 1);
  
-
+console.log("Welcome to number Guessing game");
  
  const answers = await inquirer.prompt([
   { 
